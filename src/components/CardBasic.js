2d27060bc95b25040b646c76f3891a1411b56e07
chris-ac-malone <chris.ac.malone@gmail.com>
2023-01-17 23:28:21 -0700

    Added dynamic content for cards. Header Styling.

--- src/components/CardBasic.js
+++ src/components/CardBasic.js
@@ -1,10 +1,10 @@
 function CardBasic(props) {
   return (
-    <a href="https://codepen.io/Acedoutry/pen/zYaOgRX" target="_blank">
-      <div className="card text-xl rounded-xl shadow-md shadow-black/30 relative cursor-pointer text-white bg-slate-50 opacity-90 bg-center bg-unzoom hover:bg-zoom transition-all bg-cm-3d-card bg-blend-multiply aspect-square max-w-none m-3">
-        <div className="title w-full bottom-0 pointer-events-none absolute p-2 border-t-2 border-slate-700 bg-slate-50 dark:bg-slate-500 rounded-b-xl bg-opacity-40 z-10 font-bold text-2xl">{props.cardTitle}</div>
-        <div className="overlay w-full rounded-xl bg-black/30 absolute"></div>
-        <div className="content rounded-xl font-semibold p-10 h-full hover:opacity-100 md:opacity-0 transition-opacity w-full text-white bg-black bg-opacity-60 absolute top-0 left-0">{props.cardInfo}</div>
+    <a href={props.href} target="_blank">
+      <div style={{ backgroundImage: `url(${props.bgImage})` }} className="card text-xl rounded shadow-md shadow-black/30 relative cursor-pointer text-white bg-slate-50 opacity-90 bg-center bg-unzoom hover:bg-zoom transition-all bg-blend-multiply aspect-square max-w-none m-3">
+        <div className="title w-full bottom-0 pointer-events-none absolute p-2 border-t-2 border-slate-700 bg-slate-50 dark:bg-slate-500 rounded-b bg-opacity-40 z-10 font-bold text-2xl">{props.cardTitle}</div>
+        <div className="overlay w-full rounded bg-black/30 absolute"></div>
+        <div className="content rounded font-semibold p-10 h-full hover:opacity-100 md:opacity-0 transition-opacity w-full text-white bg-black bg-opacity-60 absolute top-0 left-0">{props.cardInfo}</div>
       </div>
     </a>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
 import CardBasic from "../components/CardBasic";
+import Profile from "../assets/Chris Graduation-25.jpg";
 
 const Home = () => {
     return (
         <>
-            <div className="bg-teal-50 h-screen dark:bg-slate-500">
-                <div className="text-center text-4xl font-bold"><h1 className="py-10 bg-teal-50 dark:bg-slate-500">ChrisMalone.dev</h1></div>
-                <div className="mx-auto px-3 grid md:grid-cols-4 grid-cols-1 container text-neutral-900 dark:text-neutral-50">
-                    <CardBasic cardTitle="CM 3D Card" cardInfo="A 3d Card widget that generates cards on valid divs on the page. These cards use Javascript-driven-CSS to show a 3D effect on hover." />
-                    <CardBasic cardTitle="Title" cardInfo="asd" />
-                    <CardBasic cardTitle="Title" cardInfo="asd" />
-                    <CardBasic cardTitle="Title" cardInfo="asd" />
+            <div className="bg-blue-200/80 dark:bg-slate-500">
+                <div className="h-screen w-full table text-center text-4xl font-bold">
+                    <h1 className="table-cell w-full text-center align-middle py-10 dark:bg-slate-500 text-7xl">ChrisMalone.dev<img className="m-auto mt-10 w-40 rounded-full shadow-lg" src={Profile} alt="Chris Malone Web Developer" /></h1>
+                </div>
+                <div className="relative mt-20">
+                    <div className="text-9xl mix-blend-difference text-gray-100 absolute -top-20">Portfolio</div>
+                </div>
+                <div className="mx-auto px-3 grid lg:grid-cols-3 xl:grid-cols-3 grid-cols-1  container text-neutral-900 dark:text-neutral-50">
+                    <CardBasic bgImage={require('../assets/cm-3d-card.png')} href="https://codepen.io/Acedoutry/pen/zYaOgRX" cardTitle="CM 3D Card" cardInfo="A 3d Card widget that generates cards on valid divs on the page. These cards use Javascript-driven-CSS to show a 3D effect on hover." />
+                    <CardBasic bgImage={require('../assets/vertical-carousel.png')} href="https://codepen.io/Acedoutry/full/YzjQXee" cardTitle="Vertical Carousel" cardInfo="A 3D slot-style vertical carousel. It rotates on scroll and locks into the next item. In progress." />
+                    <CardBasic bgImage={require('../assets/cm-3d-card.png')} href="https://codepen.io/Acedoutry/pen/zYaOgRX" cardTitle="CM 3D Card" cardInfo="A 3d Card widget that generates cards on valid divs on the page. These cards use Javascript-driven-CSS to show a 3D effect on hover." />
+                </div>
+                <div className="mx-auto px-3 grid lg:grid-cols-2 xl:grid-cols-4 grid-cols-1 container text-neutral-900 dark:text-neutral-50">
+                    <CardBasic bgImage={require('../assets/cm-3d-card.png')} href="https://codepen.io/Acedoutry/pen/zYaOgRX" cardTitle="CM 3D Card" cardInfo="A 3d Card widget that generates cards on valid divs on the page. These cards use Javascript-driven-CSS to show a 3D effect on hover." />
+                    <CardBasic bgImage={require('../assets/vertical-carousel.png')} href="https://codepen.io/Acedoutry/full/YzjQXee" cardTitle="Vertical Carousel" cardInfo="A 3D slot-style vertical carousel. It rotates on scroll and locks into the next item. In progress." />
+                    <CardBasic bgImage={require('../assets/vertical-carousel.png')} href="https://codepen.io/Acedoutry/full/YzjQXee" cardTitle="Vertical Carousel" cardInfo="A 3D slot-style vertical carousel. It rotates on scroll and locks into the next item. In progress." />
+                    <CardBasic bgImage={require('../assets/vertical-carousel.png')} href="https://codepen.io/Acedoutry/full/YzjQXee" cardTitle="Vertical Carousel" cardInfo="A 3D slot-style vertical carousel. It rotates on scroll and locks into the next item. In progress." />
                 </div>
             </div>
         </>
